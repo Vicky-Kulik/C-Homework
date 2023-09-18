9d3d6a457bb42df480e307490030603f72ac4213
Vicky-Kulik <113286997+Vicky-Kulik@users.noreply.github.com>
2023-09-18 23:44:04 +0300

    Homework3

---
+++ Task3_2.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+double GetDistanceToPoint3D (int X1, int Y1, int Z1, int X2, int Y2, int Z2)
+{
+    double distance = Math.Sqrt( Math.Pow(X2-X1, 2) + Math.Pow(Y2-Y1, 2) + Math.Pow(Z2-Z1, 2));
+
+    return distance;
+}
+
+Console.WriteLine ("Введите X1: ");
+int X1 = int.Parse (Console.ReadLine());
+
+Console.WriteLine ("Введите Y1: ");
+int Y1 = int.Parse (Console.ReadLine());
+
+Console.WriteLine ("Введите Z1: ");
+int Z1 = int.Parse (Console.ReadLine());
+
+Console.WriteLine ("Введите X2: ");
+int X2 = int.Parse (Console.ReadLine());
+
+Console.WriteLine ("Введите Y2: ");
+int Y2 = int.Parse (Console.ReadLine());
+
+Console.WriteLine ("Введите Z2: ");
+int Z2 = int.Parse (Console.ReadLine());
+
+double result = GetDistanceToPoint3D (X1, Y1, Z1, X2, Y2, Z2);
+Console.WriteLine ($"Расстояние между точками ({X1}; {Y1}; {Z1}) и ({X2}; {Y2}; {Z2})  равно {Math.Round(result, 2)}");
---
+++ Task3_3.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int[] GetCubesNum(int number)
+{
+    int[] cubeNum = new int [number];
+    for (int i=0; i < number; i++)
+    {
+        cubeNum[i] = i*i*i;
+    }
+
+    return cubeNum;
+}
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int[] result = GetCubesNum(num);
+for (int i = 0; i < result.Length; i++)
+{
+    Console.WriteLine(result[i]);
+}
